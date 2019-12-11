57dbb03f31cb72fd0d13560e84aa192da7a412cd
Scott Lester <scott.lester@6point6.co.uk>
2019-12-11 11:59:39 +0000

    Added functions for checking records

--- mail_check.py
+++ mail_check.py
@@ -1,16 +1,13 @@
 import os, sys
 import checkdmarc
 
-def check(domain):
-    print("Checking \"%s\"" % domain)
-
-    pure_domain = checkdmarc.get_base_domain(domain)
-    print("Pure domain = \"%s\"" % pure_domain)
+def check_DMARC(domain):
+    print("Checking DMARC for \"%s\"" % domain)
 
     dmarc = {}
     
     try:
-        dmarc = checkdmarc.get_dmarc_record(pure_domain, timeout=10.0, nameservers=["8.8.8.8", "1.1.1.1"])
+        dmarc = checkdmarc.get_dmarc_record(domain, timeout=10.0, nameservers=["8.8.8.8", "1.1.1.1"])
         
         #print(dmarc)
 
@@ -23,6 +20,9 @@ def check(domain):
         # parsed
         parsed = dmarc["parsed"]
 
+        # version
+        print("Version: %s" % parsed["tags"]["v"]["value"])
+
         # get DKIM
         print("DKIM explicit: %s" % parsed["tags"]["adkim"]["explicit"])
         
@@ -32,13 +32,66 @@ def check(domain):
             print("SPF Value: %s" % parsed["tags"]["aspf"]["value"])
         
         # print all the dicts anyway
-        print("\nAll data:")
-        for key in parsed["tags"]:
-           print("%s: %s" % (key, parsed["tags"][key])) 
+        #print("\nAll data:")
+        #for key in parsed["tags"]:
+           #print("%s: %s" % (key, parsed["tags"][key]))
+        
+        print("\n")
 
     except Exception as e:
         print('Error with ' + domain)
         print(e)
+
+
+# TODO fix this
+def check_domain(domain):
+    print("Checking domain records for \"%s\"" % domain)
+    dmarc = {}
+    
+    try:
+        dmarc = checkdmarc.check_domains(domain, timeout=10.0, nameservers=["8.8.8.8", "1.1.1.1"])
+        
+        print(dmarc)
+    except Exception as e:
+            print('Error with ' + domain)
+            print(e)
+    
+    print("\n")
+
+
+def get_hosts(domain):
+    dmarc = {}
+    
+    try:
+        dmarc = checkdmarc.get_mx_hosts(domain, timeout=10.0, nameservers=["8.8.8.8", "1.1.1.1"])
+        
+        print(dmarc)
+
+        # TODO parse this
+    except Exception as e:
+            print('Error with ' + domain)
+            print(e)
+
+    print("\n")
+
+
+def check_SPF(pure_domain):
+    dmarc = {}
+    
+    try:
+        dmarc = checkdmarc.query_spf_record(domain, timeout=10.0, nameservers=["8.8.8.8", "1.1.1.1"])
+        
+        print("SPF Record: %s" % dmarc["record"])
+
+        #print("Warning: %s" % dmarc["warnings"])
+
+        # TODO parse this
+    except Exception as e:
+            print('Error with ' + domain)
+            print(e)
+    
+    print("\n")
+
 
 if __name__ == '__main__':
     if len(sys.argv) is not 2:
@@ -46,4 +99,12 @@ if __name__ == '__main__':
         sys.exit(-1)
     
     domain = sys.argv[1]
-    check(domain)
+
+    pure_domain = checkdmarc.get_base_domain(domain)
+    print("Pure domain = \"%s\"" % pure_domain)
+
+    get_hosts(pure_domain)
+    check_DMARC(pure_domain)
+    check_domain(pure_domain)
+    check_SPF(pure_domain)
+    
