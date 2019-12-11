d98d9dcb00572a96072ec485b949dbba0fc892f3
Scott Lester <scott.lester@6point6.co.uk>
2019-12-11 10:55:29 +0000

    Got it all printing

--- mail_check.py
+++ mail_check.py
@@ -8,17 +8,33 @@ def check(domain):
     print("Pure domain = \"%s\"" % pure_domain)
 
     dmarc = {}
+    
     try:
         dmarc = checkdmarc.get_dmarc_record(pure_domain, timeout=10.0, nameservers=["8.8.8.8", "1.1.1.1"])
         
         #print(dmarc)
 
-        # record
-        for key in dmarc["record"]:
-            
+        # location
+        print("Location: %s" % dmarc["location"])
 
-        for dict_name in dmarc:
-            print(dict_name)
+        # record
+        print("Raw Record: %s" % dmarc["record"])
+
+        # parsed
+        parsed = dmarc["parsed"]
+
+        # get DKIM
+        print("DKIM explicit: %s" % parsed["tags"]["adkim"]["explicit"])
+        
+        # Get SPF
+        print("SPF explicit: %s" % parsed["tags"]["aspf"]["explicit"])
+        if parsed["tags"]["aspf"]["explicit"] is True:
+            print("SPF Value: %s" % parsed["tags"]["aspf"]["value"])
+        
+        # print all the dicts anyway
+        print("\nAll data:")
+        for key in parsed["tags"]:
+           print("%s: %s" % (key, parsed["tags"][key])) 
 
     except Exception as e:
         print('Error with ' + domain)
